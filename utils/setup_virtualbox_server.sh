c7677cdbe0426ca202cec008a151860d2f52352e
SRE Challenge <srechallenge@outlook.com>
2017-10-08 20:56:31 -0400

    Updating setup_virtualbox_server.sh fixing gzip rpm name

--- utils/setup_virtualbox_server.sh
+++ utils/setup_virtualbox_server.sh
@@ -3,7 +3,7 @@
 #Bootstrap server vm
 
 yum makecache
-yum install -y git gunzip perl nmap kernel-devel gcc wget PyYAML
+yum install -y git gzip perl nmap kernel-devel gcc wget PyYAML
 yum install -y https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.rpm
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import -
 curl http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -o /etc/yum.repos.d/virtualbox.repo
