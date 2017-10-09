833c28f40e3633041bae86cd5b76b55907a76589
SRE Challenge <srechallenge@outlook.com>
2017-10-08 20:54:00 -0400

    Updating setup_virtualbox_server.sh to add PyYAML

--- utils/setup_virtualbox_server.sh
+++ utils/setup_virtualbox_server.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 #Bootstrap server vm
+
+yum makecache
+yum install -y git gunzip perl nmap kernel-devel gcc wget PyYAML
+yum install -y https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.rpm
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import -
 curl http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -o /etc/yum.repos.d/virtualbox.repo
-yum makecache
-yum install -y git gunzip perl nmap kernel-devel gcc
-yum install -y https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.rpm
 yum install -y VirtualBox-5.1
